dfff53f066aef799609f5fe0be832ee9c0f80b9d
jeman <jemanp96@gmail.com>
2022-10-25 22:20:18 -0400

    Update api_practice_get_post_delete.py
    I completed get_cities getting time zone from api

--- api_practice_get_post_delete.py
+++ api_practice_get_post_delete.py
@@ -11,8 +11,8 @@ class City(BaseModel):
     timezone: str
     
 @app.get("/")
-    def root():
-        return {"hello": "world"}
+def root():
+    return {"hello": "world"}
 
 @app.get('/cities')
 def get_cities():
@@ -20,17 +20,27 @@ def get_cities():
     for city in db:
         strs = "http://worldtimeapi.org/api/timezone/America/New_York"
         r = requests.get(strs) # get ip address 
+        cur_time = r.json()['datatime']
+        results.append({'name':city['name'],'timezone':city['timezone'] , 'current_time':cur_time})
     
     return result #return city created
 
 
 @app.get('/cities/{city_id}')
 def get_city(city_id: int):
+    city  = db[city_id-1]
+    strs = f"http://worldtimeapi.org/api/timezone/America/New_York/{city['timezone']}"
+    r = requests.get(strs) # get ip address 
+    cur_time = r.json()['datatime']
+    return {'name':city['name'],'timezone':city['timezone'] , 'current_time':cur_time}
+    #return the list of dictionary 
     
 @app.get('/cities')
-def create_city(city_id: int):
+def create_city(city: City):
+    db.append(city.dict())
+    return db[-1] 
 
-@app.get('/cities/{city_id}')
-def delete_city(city_id: int):
+#@app.get('/cities/{city_id}')
+#def delete_city(city_id: int):
     
     
