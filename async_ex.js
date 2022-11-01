64675c7d8a94562c7553cb4612e5c4ef93f1912d
jeman <jemanp96@gmail.com>
2022-10-31 22:00:44 -0400

    Create async_ex.js
    leanred about async and await syntax and how they works

---
+++ async_ex.js
@@ -0,0 +1,13 @@
+function fetchUser() {
+    var url = 'https://jsonplaceholder.typicode.com/users/1'
+    return fetch(url).then(function(response) {
+      return response.json();
+    });
+  }
+  
+  function fetchTodo() {
+    var url = 'https://jsonplaceholder.typicode.com/todos/1';
+    return fetch(url).then(function(response) {
+      return response.json();
+    });
+  }
